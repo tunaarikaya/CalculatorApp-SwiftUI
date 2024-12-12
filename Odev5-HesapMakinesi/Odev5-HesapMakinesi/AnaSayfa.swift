07dc2d7e782bed0d9ed02ef5342f93b33f5051fe
Mehmet Tuna Arıkaya <m.tunaarikaya@gmail.com>
2024-12-13 00:53:47 +0300

    main
    main

---
+++ Odev5-HesapMakinesi/Odev5-HesapMakinesi/AnaSayfa.swift
@@ -0,0 +1,106 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State private var displayText = "0"
+    @State private var currentInput = ""
+    @State private var storedValue: Double? = 0.0
+    @State private var currentOperator: String?
+    
+    var body: some View {
+        VStack {
+            Text(displayText)
+                .font(.system(size: 70))
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .padding()
+            
+            Spacer()
+            
+            // Örnek buton satırları
+            CalculatorButtonRow(buttons: ["7", "8", "9", "+"], action: buttonTap)
+            CalculatorButtonRow(buttons: ["4", "5", "6", "-"], action: buttonTap)
+            CalculatorButtonRow(buttons: ["1", "2", "3", "*"], action: buttonTap)
+            CalculatorButtonRow(buttons: ["0", ".", "=", "/"], action: buttonTap)
+            CalculatorButtonRow(buttons: ["C"], action: {button in clearButton() })
+        }
+        .padding()
+    }
+    
+    
+    func clearButton(){
+        currentInput = ""
+        storedValue = nil
+        currentInput = ""
+        displayText = "0"
+    }
+    
+    
+    func buttonTap(_ button: String) {
+        switch button {
+        case "+", "-", "*", "/":
+            currentOperator = button
+            storedValue = Double(currentInput)
+            currentInput = ""
+        case "=":
+            if let operatorType = currentOperator,
+               let storedValue = storedValue,
+               let currentValue = Double(currentInput) {
+                let result: Double
+                switch operatorType {
+                case "+":
+                    result = storedValue + currentValue
+                case "-":
+                    result = storedValue - currentValue
+                case "*":
+                    result = storedValue * currentValue
+                case "/":
+                    result = storedValue / currentValue
+                default:
+                    fatalError("Unknown operator")
+                }
+                currentInput = "\(result)"
+                currentOperator = nil
+            }
+        case ".":
+            if !currentInput.contains(".") {
+                currentInput += button
+            }
+        default:
+            currentInput += button
+        }
+        displayText = currentInput
+        
+        if (currentInput.isEmpty) {
+            displayText = "0"
+        }else{
+            displayText = currentInput
+        }
+    }
+   
+}
+
+struct CalculatorButtonRow: View {
+    let buttons: [String]
+    let action: (String) -> Void
+    let buttonSize = (UIScreen.main.bounds.width - 5 * 12) / 4
+    
+    var body: some View {
+        HStack(spacing: 12) {
+            ForEach(buttons, id: \.self) { buttonLabel in
+                Button {
+                    action(buttonLabel)
+                } label: {
+                    Text(buttonLabel)
+                        .font(.title)
+                        .frame(width: buttonSize, height: buttonSize)
+                        .background(Color.orange)
+                        .foregroundStyle(.black)
+                        .cornerRadius(buttonSize / 2)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Odev5-HesapMakinesi/Odev5-HesapMakinesi/Odev5_HesapMakinesiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Odev5_HesapMakinesiApp.swift
+//  Odev5-HesapMakinesi
+//
+//  Created by Mehmet Tuna Arıkaya on 12.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Odev5_HesapMakinesiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
