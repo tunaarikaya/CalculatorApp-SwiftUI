2efd95056e6f4b0e0e454ac4f11d2d84376c65b5
Tuna Arikaya <155156717+tunaarikaya@users.noreply.github.com>
2025-01-27 16:12:27 +0300

    Update AnaSayfa.swift

--- Odev5-HesapMakinesi/Odev5-HesapMakinesi/AnaSayfa.swift
+++ Odev5-HesapMakinesi/Odev5-HesapMakinesi/AnaSayfa.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
                 .frame(maxWidth: .infinity, alignment: .trailing)
                 .padding()
             
-            //1a
+            //1aa
             CalculatorButtonRow(buttons: ["7", "8", "9", "+"], action: buttonTap)
             CalculatorButtonRow(buttons: ["4", "5", "6", "-"], action: buttonTap)
             CalculatorButtonRow(buttons: ["1", "2", "3", "*"], action: buttonTap)
